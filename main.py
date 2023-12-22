743a23f6a73b37221a54f4192e1b20f79512a2d0
Leonov Egor <crozetle@gmail.com>
2023-12-22 11:37:40 +0400

    1. Обновил

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt3
 import matplotlib.pyplot as plt4
 import math
 
+
 class Link:
 
     def stepR(self):
@@ -20,7 +21,9 @@ class Link:
 
 
 def findModule(x, y):
-    return math.sqrt(x**2 + y**2)
+    return math.sqrt(x ** 2 + y ** 2)
+
+
 class Aper:
 
     def makeStep(self, k, T):
@@ -51,45 +54,56 @@ class Aper:
             xp.append(w)
         return xp, yp
 
-    def stepR(self):
+    def changeK(self):
         T = 3
         for k in 1, 3, 5:
             x, y = self.makeStep(k, T)
-            plt.plot(x, y, label=f"plot k = {k}", marker='o', markersize=4)
-        plt.title("Апериодическое звено - Переходная характеристика - Изменение к")
-        plt.legend(("k = 1", "k = 3", "k = 5"))
-        plt.savefig("stepR k")
-        plt.clf()
+            plt1.plot(x, y, label=f"plot k = {k}", marker='o', markersize=4)
+            x, y = self.makeAFCH(k, T)
+            plt2.plot(x, y, label=f"plot k = {k}", marker='o', markersize=4)
+            x, y = self.makeACH(k, T)
+            plt3.plot(x, y, label=f"plot k = {k}", marker='o', markersize=4)
 
+        plt1.title("Апериодическое звено - Переходная характеристика - Изменение к")
+        plt1.legend(("k = 1", "k = 3", "k = 5"))
+        plt1.savefig("Переходная характеристика - К")
+        plt1.clf()
+
+        plt2.title("Апериодическое звено - АФЧХ - Изменение к")
+        plt2.legend(("k = 1", "k = 3", "k = 5"))
+        plt2.savefig("АФЧХ - К")
+        plt2.clf()
+
+        plt3.title("Апериодическое звено - АЧХ - Изменение к")
+        plt3.legend(("k = 1", "k = 3", "k = 5"))
+        plt3.savefig("АЧХ - К")
+        plt3.clf()
+
+    def changeT(self):
         k = 3
         for T in 1, 3, 5:
             x, y = self.makeStep(k, T)
-            plt.plot(x, y, label=f"plot k = {k}", marker='o', markersize=4)
-        plt.title("Апериодическое звено - Переходная характеристика - Изменение Т")
-        plt.legend(("T = 1", "T = 3", "T = 5"))
-        plt.savefig("stepR T")
-        plt.clf()
-
-    def afch(self):
-        T = 3
-        for k in 1, 3, 5:
+            plt1.plot(x, y, label=f"plot T = {k}", marker='o', markersize=4)
             x, y = self.makeAFCH(k, T)
-            plt.plot(x, y, label=f"plot k = {k}", marker='o', markersize=4)
-        plt.title("Апериодическое звено - АФЧХ - Изменение к")
-        plt.legend(("k = 1", "k = 3", "k = 5"))
-        plt.savefig("afch k")
-        plt.clf()
+            plt2.plot(x, y, label=f"plot T = {k}", marker='o', markersize=4)
+            x, y = self.makeACH(k, T)
+            plt3.plot(x, y, label=f"plot T = {k}", marker='o', markersize=4)
 
-        k = 3
-        for T in 1, 3, 5:
-            x, y = self.makeAFCH(k, T)
-            plt.plot(x, y, label=f"plot k = {k}", marker='o', markersize=4)
-        plt.title("Апериодическое звено - АФЧХ - Изменение Т")
-        plt.legend(("T = 1", "T = 3", "T = 5"))
-        plt.savefig("afch T")
-        plt.clf()
+        plt1.title("Апериодическое звено - Переходная характеристика - Изменение T")
+        plt1.legend(("T = 1", "T = 3", "T = 5"))
+        plt1.savefig("Переходная характеристика - T")
+        plt1.clf()
 
-ap = Aper
-ap.stepR()
-ap.afch()
+        plt2.title("Апериодическое звено - АФЧХ - Изменение T")
+        plt2.legend(("T = 1", "T = 3", "T = 5"))
+        plt2.savefig("АФЧХ - T")
+        plt2.clf()
 
+        plt3.title("Апериодическое звено - АЧХ - Изменение T")
+        plt3.legend(("T = 1", "T = 3", "T = 5"))
+        plt3.savefig("АЧХ - T")
+        plt3.clf()
+
+
+Aper.changeT(Aper)
+Aper.changeK(Aper)
